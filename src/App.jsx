f97b857b0d5c3bbfcb6447bb18d720e21995de47
Quake Patel <quake.patel@gmail.com>
2025-12-14 21:21:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ import UploadFile from "./pages/UploadFile.jsx";
 
 export default function App() {
   return (
-    <BrowserRouter >
+    <BrowserRouter basename="/">
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Login />} /> 
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/employee" element={<EmployeeDashboard />} />
         <Route path="/upload" element={<UploadFile  />} />
