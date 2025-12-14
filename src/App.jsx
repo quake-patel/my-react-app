56c66c5324234783b286fb8c9be34e1d06d20392
Quake Patel <quake.patel@gmail.com>
2025-12-14 21:38:57 +0530

    c

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import UploadFile from "./pages/UploadFile.jsx";
 
 export default function App() {
   return (
-    <BrowserRouter basename="/my-react-app" >
+    <BrowserRouter baseurl="/my-react-app" >
       <Routes>
         <Route path="/" element={<Login />} /> 
         <Route path="/admin" element={<AdminDashboard />} />
